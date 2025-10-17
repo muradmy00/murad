53c65aa2015cebf22700162aa6b274495628dfce
Md Mohiuddin Murad Miraj <mohiuddinmurad.my00@gmail.com>
2025-10-17 18:26:05 +0000

    added skills...it's my skill

--- src/components/icons/skill-icon.tsx
+++ src/components/icons/skill-icon.tsx
@@ -9,6 +9,23 @@ import {
   SiSolidity,
   SiGit,
   SiFigma,
+  SiHtml5,
+  SiCss3,
+  SiRedux,
+  SiReactrouter,
+  SiGithub,
+  SiNpm,
+  SiVite,
+  SiYarn,
+  SiC,
+  SiCplusplus,
+  SiJava,
+  SiPython,
+  SiReacthookform,
+  SiZustand,
+  SiQuill,
+  SiExpo,
+  SiAxios,
 } from '@icons-pack/react-simple-icons';
 
 type SkillIconProps = {
@@ -18,6 +35,7 @@ type SkillIconProps = {
 
 export const SkillIcon = ({ skillName, className }: SkillIconProps) => {
   switch (skillName) {
+    case 'React.js':
     case 'React':
       return <SiReact className={className} color="#61DAFB" />;
     case 'Next.js':
@@ -32,10 +50,46 @@ export const SkillIcon = ({ skillName, className }: SkillIconProps) => {
       return <SiNodedotjs className={className} color="#339933" />;
     case 'Solidity':
         return <SiSolidity className={className} color="#363636" />;
-    case 'Git & GitHub':
+    case 'Git':
       return <SiGit className={className} color="#F05032" />;
     case 'Figma':
       return <SiFigma className={className} color="#F24E1E" />;
+    case 'HTML':
+        return <SiHtml5 className={className} color="#E34F26" />;
+    case 'CSS':
+        return <SiCss3 className={className} color="#1572B6" />;
+    case 'Redux':
+        return <SiRedux className={className} color="#764ABC" />;
+    case 'React Native':
+        return <SiReact className={className} color="#61DAFB" />;
+    case 'React Router DOM':
+        return <SiReactrouter className={className} color="#CA4245" />;
+    case 'GitHub':
+        return <SiGithub className={className} color="#FFFFFF" />;
+    case 'NPM':
+        return <SiNpm className={className} color="#CB3837" />;
+    case 'Vite':
+        return <SiVite className={className} color="#646CFF" />;
+    case 'Yarn':
+        return <SiYarn className={className} color="#2C8EBB" />;
+    case 'C':
+        return <SiC className={className} color="#A8B9CC" />;
+    case 'C++':
+        return <SiCplusplus className={className} color="#00599C" />;
+    case 'Java':
+        return <SiJava className={className} color="#007396" />;
+    case 'Python':
+        return <SiPython className={className} color="#3776AB" />;
+    case 'React Hook Form':
+        return <SiReacthookform className={className} color="#EC5990" />;
+    case 'Zustand':
+        return <SiZustand className={className} color="#000000" />;
+    case 'Quill':
+        return <SiQuill className={className} color="#00A9E2" />;
+    case 'Expo':
+        return <SiExpo className={className} color="#000020" />;
+    case 'Axios':
+        return <SiAxios className={className} color="#5A29E4" />;
     default:
       return null;
   }
