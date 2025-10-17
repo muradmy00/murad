7fc76e4d76124c4a8b36b088b53d6dad376408a9
Md Mohiuddin Murad Miraj <mohiuddinmurad.my00@gmail.com>
2025-10-17 14:03:08 +0000

    Header and footer a name er pase M letter desin kore use koren

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -11,11 +11,9 @@ const Logo = (props: SVGProps<SVGSVGElement>) => (
     strokeLinejoin="round"
     {...props}
   >
-    <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
-    <polyline points="14 2 14 8 20 8" />
-    <line x1="16" y1="13" x2="8" y2="13" />
-    <line x1="16" y1="17" x2="8" y2="17" />
-    <polyline points="10 9 9 9 8 9" />
+    <path d="M6 3v18l6-6 6 6V3L6 3z" />
+    <path d="M12 9l-6 6" />
+    <path d="M12 9l6 6" />
   </svg>
 );
 
